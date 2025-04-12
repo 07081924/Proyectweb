f308edc94bc5b6ffb18529d6fe5ec94b5ed2a8fa
07081924 <jesus07oquendo@gmail.com>
2025-04-12 11:08:46 -0500

    Add files via upload

---
+++ js/tabla.js
@@ -0,0 +1,5 @@
+const url = 'https://script.google.com/macros/s/AKfycbwyXLJUzplMfy88RRa-G1s1Bzxv1SzW0j175aABT9Zg_K55Rsiopbg4WNqofAiqmtxS/exec'; // Sustituye con la URL del web app que obtuviste en Google Apps Script
+
+{
+  
+  
